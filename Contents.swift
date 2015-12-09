5ed5b7a60f336c3147779e423c3683064624919d
Jac0bAnderson <jake912235@gmail.com>
2015-12-09 09:54:24 -0700

    swift

---
+++ Contents.swift
@@ -0,0 +1,62 @@
+//: Ԁlɐʎƃɹonup - uonu: ɐ dlɐɔǝ ʍɥǝɹǝ dǝodlǝ ɔɐu dlɐʎ
+import Cocoa
+
+var  ǝlʇᴉʇ = "Mini Swift Project"
+
+class Ⅎǝɹɹǝʇs
+{
+    var ferretHandbook : Bool = true
+    var ferretAge : Int = 3
+    var ferretBreed : String = "panda"
+    let ferretLikesPeople : Bool = false    let ferretName : String = "Y tho"
+    
+
+    if  ferretLikesPeople  ==  true
+    {
+        print("Your ferret" ferretName  "is friendly")
+    }
+    else
+    {
+        print("Your ferret is friendly")
+      whyYourFerretIsBad()
+    }
+}
+
+func whyYourFerretIsBad()
+{
+    var whyItIs : Int = Int(rand()*5)
+    if whyItIs == 0
+    {
+        print("because it just is")
+    }
+    else if(whyItIs == 1)
+    {
+        print("because you don't feed it")
+    }
+    else if(whyItIs == 2)
+    {
+        print("because you're rude")
+    }
+    else if(whyItIs == 3)
+    {
+        print("because you don't give it memes")
+    }
+    else if (whyItIs == 4)
+    {
+        print("because you don't give it treats")
+    }
+}
+var gear : [String] = ["backpack", "tent", "pocket knife", "rain coat" ]
+print(gear)
+
+
+struct WayONE
+{
+    var CoolValue: Int
+}
+var coolValue = 12
+
+struct WayTWo
+{
+    
+}
